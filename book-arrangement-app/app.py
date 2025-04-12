9c24909eac097cca01168e18897e30d5f54c16d3
San <66519921+sana2242@users.noreply.github.com>
2025-04-12 18:21:16 +0300

    Add permutation calculation logic

---
+++ book-arrangement-app/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, jsonify
+import numpy as np
+
+app = Flask(__name__)
+
+
+def permutation_dp(n, r):
+    
+    dp = np.zeros((n + 1, r + 1), dtype=int)
+
+
+    for i in range(n + 1):
+        dp[i][0] = 1
+
+
+    for i in range(1, n + 1):
+        for j in range(1, min(r + 1, i + 1)):
+            if j == i:
+                dp[i][j] = i * dp[i - 1][j - 1]
+            else:
+                dp[i][j] = j * dp[i - 1][j - 1] + dp[i - 1][j]
+
+
+    dp_list = dp.tolist()
+
+    return dp_list, int(dp[n][r])
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
